ba16d20152ac56a256873dbcbd3d3f21f42e5183
Alexandr T <leshrac88@gmail.com>
2014-06-25 18:31:12 +0400

    Add style for hr.cut

--- cut/plugin.js
+++ cut/plugin.js
@@ -1,45 +1,12 @@
-// ( function() {
-//   var horizontalruleCmd = {
-//     canUndo: false, // The undo snapshot will be handled by 'insertElement'.
-//     exec: function( editor ) {
-//       var cut = editor.document.createElement( 'cut' );
-//       editor.insertElement( cut );
-//     },
-
-//     allowedContent: 'cut',
-//     requiredContent: 'cut'
-//   };
-
-//   var pluginName = 'cut';
-
-//   // Register a plugin named "cut".
-//   CKEDITOR.plugins.add( pluginName, {
-//     hidpi: true, // %REMOVE_LINE_CORE%
-//     init: function( editor ) {
-//       if ( editor.blockless )
-//         return;
-
-//       editor.addCommand( pluginName, horizontalruleCmd );
-//       editor.ui.addButton && editor.ui.addButton( 'cut', {
-//         label: "cut",
-//         command: pluginName
-//       } );
-//     }
-//   } );
-// } )();
-
-
-
-
 CKEDITOR.plugins.add( 'cut',
 {
-  hidpi: true, // %REMOVE_LINE_CORE%
+  hidpi: true,
   init: function( editor )
   {
     if ( editor.blockless )
       return;
 
-    CKEDITOR.addCss("cut {display:block; height: 2px;width: 100%;background: #DDDD99;margin-bottom: 1em;}");
+    CKEDITOR.addCss("hr.cut {display:block; height: 2px;width: 100%;background: #DDDD99;margin-bottom: 1em;}");
 	  CKEDITOR.dtd['cut']={};
     CKEDITOR.dtd.$empty['cut']=1;
     CKEDITOR.dtd.$nonEditable['cut']=1;
